75a5f79f465ae6193ce0b26d01e9892d26c83418
Sara Öhman <saraohman97@gmail.com>
2024-02-29 19:51:10 +0100

    Middleware authentication.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = async () => {
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
                 <StoreSwitcher items={stores} />
-                <MainNav className="mx-6" />
+                <MainNav className="mx-6 py-4 overflow-scroll" />
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton afterSignOutUrl="/" />
                 </div>
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/anyone-can-visit-this-route'],
+  // publicRoutes: ['/anyone-can-visit-this-route'],
+  publicRoutes: ["/api/:path*"],
   ignoredRoutes: ['/no-auth-in-this-route'],
 });
  
