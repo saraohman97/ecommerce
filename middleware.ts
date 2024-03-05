8bf0d1c09f3bc7bf6d659e3894114f53bc3b9ea3
Sara Öhman <saraohman97@gmail.com>
2024-03-05 13:04:22 +0100

    Stripe done.

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-  // publicRoutes: ['/anyone-can-visit-this-route'],
   publicRoutes: ["/api/:path*"],
-  ignoredRoutes: ['/no-auth-in-this-route'],
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
